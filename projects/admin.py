b1ce1112cf060fcc47e007ca172605024c60d41b
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-04-30 15:08:39 +0100

    Refactor project URLs and views in projects app

--- projects/admin.py
+++ projects/admin.py
@@ -1,2 +1,12 @@
-
 # Register your models here.
+# projects/admin.py
+
+from django.contrib import admin
+from projects.models import Project
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Project, ProjectAdmin)
