b3a50d080b9676cd8c18ee7e594828a53ae20a61
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-04-30 14:09:34 +0100

    Refactor project model in admin.py and models.py

--- pages/admin.py
+++ pages/admin.py
@@ -1,11 +1,3 @@
+from django.contrib import admin
 
 # Register your models here.
-# projects/models.py
-
-from django.db import models
-
-
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
