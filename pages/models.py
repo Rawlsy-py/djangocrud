4fd0323e99ffd01e21a6ff8178491af114ea4fcc
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-04-30 14:10:59 +0100

    Refactor project model in admin.py and models.py

--- pages/models.py
+++ pages/models.py
@@ -1,10 +1,2 @@
+
 # Create your models here.
-# projects/models.py
-
-from django.db import models
-
-
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
